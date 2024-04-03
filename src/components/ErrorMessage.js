8d989f0de47d0e17401728434af6ec83e17e7b24
RomanStashuk <romanstashuk1@gmail.com>
2024-04-03 21:41:40 +0300

    formated ErrorMessage code

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -2,8 +2,16 @@ import { Image } from 'react-bootstrap';
 
 import errorImg from '../img/error.gif';
 
-const ErrorMessage = ({ width, height }) => {
-  return <Image src={errorImg} width={width} height={height} alt="error" />;
+const ErrorMessage = ({ width, height, className }) => {
+  return (
+    <Image
+      src={errorImg}
+      width={width}
+      height={height}
+      className={className}
+      alt="error"
+    />
+  );
 };
 
 export default ErrorMessage;
