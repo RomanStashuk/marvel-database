311f16cde20d91921929a8e50e40f46322fc2c86
RomanStashuk <roman.stashuk.01@gmail.com>
2024-07-12 23:33:37 +0300

    add service for marvel api

---
+++ src/services/MarvelService.js
@@ -0,0 +1,40 @@
+import useHttp from '../hooks/http.hook';
+
+const useMarvelService = () => {
+  const { loading, request, error } = useHttp();
+
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  const _apiKey = `apikey=${process.env.REACT_APP_MARVEL_API_KEY}`;
+  const _baseOffset = 210;
+
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const res = await request(
+      `${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`,
+    );
+
+    return res.data.results.map(_transformCharacter);
+  };
+
+  const getCharacterById = async (id) => {
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
+    return _transformCharacter(res.data.results[0]);
+  };
+
+  const _transformCharacter = (char) => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 280)}…`
+        : 'There is no description for this character',
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items,
+    };
+  };
+
+  return { loading, error, getAllCharacters, getCharacterById };
+};
+
+export default useMarvelService;
